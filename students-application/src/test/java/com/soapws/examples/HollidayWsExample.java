e322210684a20285cb4fd31e38255c1b37b5a3f5
Renata Nobre <renathanore@gmail.com>
2019-01-05 23:06:11 -0800

    testing wsdlUrl

--- students-application/src/test/java/com/soapws/examples/HollidayWsExample.java
+++ students-application/src/test/java/com/soapws/examples/HollidayWsExample.java
@@ -5,15 +5,28 @@ package com.soapws.examples;
 
 import org.junit.Test;
 
+import io.restassured.RestAssured;
+
 /**
  * @author Renata.Vieira
  *
  */
 public class HollidayWsExample {
-	
+
 	@Test
 	public void getHolidays(){
-		
+
+		String wsdlUrl = "http://www.holidaywebservice.com//HolidayService_v2/HolidayService2.asmx?wsdl";
+		String payload="<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:hs=\"http://www.holidaywebservice.com/HolidayService_v2/\">\r\n <soapenv:Body>\r\n <hs:GetHolidaysAvailable>\r\n <hs:countryCode>Canada</hs:countryCode>\r\n </hs:GetHolidaysAvailable>\r\n </soapenv:Body>\r\n</soapenv:Envelope>";
+
+		RestAssured.given()
+		.contentType("text/xml")
+		.body(payload)
+		.post(wsdlUrl)
+		.then()
+		.log()
+		.all();
+
 	}
 
 }
