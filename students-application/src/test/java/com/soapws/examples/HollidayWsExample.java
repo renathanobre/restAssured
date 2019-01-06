1cc5a69563ee75961daf95e6daa095d10c47792e
Renata Nobre <renathanore@gmail.com>
2019-01-05 22:50:33 -0800

    new class

---
+++ students-application/src/test/java/com/soapws/examples/HollidayWsExample.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.soapws.examples;
+
+import org.junit.Test;
+
+/**
+ * @author Renata.Vieira
+ *
+ */
+public class HollidayWsExample {
+	
+	@Test
+	public void getHolidays(){
+		
+	}
+
+}
